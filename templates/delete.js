0f5b100d71484c7ad369059be510125c1198a787
Kuldeep Saini <cool.kuldeep344@gmail.com>
2022-07-19 13:46:46 +0530

    Added delete.js

---
+++ templates/delete.js
@@ -0,0 +1,30 @@
+function validateAndGetFormData() {
+    var indVar = $("#index").val();
+    if (indVar === "") {
+    alert("Index is a required Value");
+    $("#index").focus();
+    return "";
+    }
+    var jsonStrObj = {
+    index: index,
+    };
+    return JSON.stringify(jsonStrObj);
+    }
+    function resetForm() {
+    $("#index").val("");
+    $("#index").focus();
+    }
+    function deleteRecord() {
+    var jsonStr = validateAndGetFormData();
+    if (jsonStr === "") {
+    return;
+    }
+    var removeReqStr = createREMOVERecordRequest("90939076|-31949282520496781|90940641", "SAMPLE_DB", "Emp-rel", parseInt(document.getElementById("index").value));
+    alert(removeReqStr);
+    jQuery.ajaxSetup({ async: false });
+    var resultObj = executeCommandAtGivenBaseUrl(removeReqStr,
+    "http://api.login2explore.com:5577", "/api/iml");
+    alert(JSON.stringify(resultObj));
+    jQuery.ajaxSetup({ async: true });
+    resetForm();
+    }
